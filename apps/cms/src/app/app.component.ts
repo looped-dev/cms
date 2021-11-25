d2e0858c3168a9d40f1efd034050782c03fa2247
Maina Wycliffe <wmmaina7@gmail.com>
2021-11-25 11:41:31 +0300

    Generate An Angular Project For the CMS

---
+++ apps/cms/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'looped-cms-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cms';
+}
