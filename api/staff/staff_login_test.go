8c7fcffd58efc4108f09e188f27adcdacda733b7
Maina Wycliffe <wmmaina7@gmail.com>
2022-02-13 22:24:36 +0300

    Fix Tests For Staff Login

--- api/staff/staff_login_test.go
+++ api/staff/staff_login_test.go
@@ -1,6 +1,7 @@
 package staff
 
 import (
+	"log"
 	"testing"
 
 	"github.com/looped-dev/cms/api/graph/model"
@@ -8,11 +9,20 @@ import (
 )
 
 func TestStaffLogin(t *testing.T) {
+	// create a login user for testing
+	staffInput := &model.StaffRegisterInput{
+		Name:     "test",
+		Email:    "login_test@example.com",
+		Password: "password",
+	}
+
+	if _, err := StaffRegister(db, staffInput); err != nil {
+		log.Fatalf("Unable to create users for testing")
+	}
+
 	_, err := StaffLogin(db, &model.StaffLoginInput{
 		Email:    "login_test@example.com",
 		Password: "password",
 	})
 	assert.NoError(t, err)
-	// assert.Equal(t, staff.Email, "login_test@example.com")
-	// assert.Equal(t, staff.Name, "test")
 }
