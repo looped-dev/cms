4f67f16fe8554860d9275f332ef2799480e96a94
Maina Wycliffe <wmmaina7@gmail.com>
2022-02-26 01:03:20 +0300

    Add Angular Apollo to the Project

--- apps/cms/src/app/app.module.ts
+++ apps/cms/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { InMemoryCache } from '@apollo/client';
+import { HttpLink } from 'apollo-angular/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -10,7 +13,20 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'http://localhost:8080/graphiql',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
