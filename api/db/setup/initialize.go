9c0c87ecdd2e7ad0aef38891edf7fd7ec9fc5199
Maina Wycliffe <wmmaina7@gmail.com>
2022-03-02 21:39:07 +0300

    Add Check For Whether DB Exists
    This runs setup methods if database doesn't exists

--- api/db/setup/initialize.go
+++ api/db/setup/initialize.go
@@ -1,6 +1,14 @@
 package setup
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/looped-dev/cms/api/db"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 func NewSetup(dbClient *mongo.Client) *Setup {
 	return &Setup{
@@ -14,7 +22,22 @@ type Setup struct {
 }
 
 // Initialize the database with the default values and indexes
-func (s *Setup) Initialize() error {
+func (s *Setup) Initialize(w io.ReadWriter, ctx context.Context) error {
+	shouldSetupDatabse, err := s.ShouldSetupDB(ctx)
+	if err != nil {
+		return fmt.Errorf("Error checking whether database exists: %v", err)
+	}
+	if !shouldSetupDatabse {
+		fmt.Fprintf(w, "✅ Database is already setup, skipping setup! \n")
+		return nil
+	}
+	// from here, we need run the database setup, this will basically prepare the
+	// database for use by looped CMS.
+	// Checklist:
+	// 1. Create the database
+	// 2. Create the indexes
+	// 3. Create capped collection for settings
+	fmt.Fprintf(w, "🔨 Creating database: %s \n", db.DefaultDatabaseName)
 	return nil
 }
 
@@ -23,7 +46,30 @@ func (s *Setup) CreateIndexes() error {
 	return nil
 }
 
-// CreateInitialStaffMember creates the initial Staff Member for the CMS
+// CreateInitialStaffMember creates the initial Staff Member for the CMS, the
+// initial user is going to be setup via the API
 func (s *Setup) CreateInitialStaffMember() error {
 	return nil
+}
+
+// if database doesn't exist create collection and add necessary indexes
+func (s *Setup) ShouldSetupDB(ctx context.Context) (bool, error) {
+	client := s.DBClient
+	listOfDatabases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		return false, err
+	}
+	// setup if there are not databases
+	if len(listOfDatabases) == 0 {
+		return true, nil
+	}
+	// check if the database being used by the CMS exists, if exists, do not setup
+	isDBFound := false
+	for _, name := range listOfDatabases {
+		if name == db.DefaultDatabaseName {
+			isDBFound = false
+			break
+		}
+	}
+	return isDBFound, nil
 }
