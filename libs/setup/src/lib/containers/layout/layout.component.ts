37fd4fc056305fba1c2f2b2508dc1ce0eb6cd66f
Maina Wycliffe <wmmaina7@gmail.com>
2022-03-13 15:30:15 +0300

    Check if Setup and Redirect to Dashboard if Setup
    #18

--- libs/setup/src/lib/containers/layout/layout.component.ts
+++ libs/setup/src/lib/containers/layout/layout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { SetupRegisterService } from '../../state/setup-register.service';
 
 @Component({
@@ -12,14 +12,18 @@ import { SetupRegisterService } from '../../state/setup-register.service';
 export class LayoutComponent {
   loadingIcon = faSpinner;
 
-  isSetup$: Observable<boolean> = this.setupRegisterService.isSiteSetup().pipe(
-    tap((isSetup) => {
-      // if cms is setup, then redirect to the home page
-      if (isSetup) {
-        this.router.navigate(['/']);
-      }
-    })
-  );
+  isSetup$: Observable<{ isSetup: boolean }> = this.setupRegisterService
+    .isSiteSetup()
+    .pipe(
+      tap((isSetup) => {
+        // if cms is setup, then redirect to the home page
+        if (isSetup) {
+          this.router.navigate(['/']);
+        }
+      }),
+      map((isSetup) => ({ isSetup: isSetup })),
+      tap((isSetup) => console.log(isSetup))
+    );
 
   constructor(
     private setupRegisterService: SetupRegisterService,
