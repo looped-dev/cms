efcc58c16396451b3d1525e0c15be51aa771182d
Maina Wycliffe <wmmaina7@gmail.com>
2022-03-13 20:22:55 +0300

    Style the Step One Step

--- libs/setup/src/lib/forms/step-one/step-one.component.ts
+++ libs/setup/src/lib/forms/step-one/step-one.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'looped-cms-step-one',
   templateUrl: './step-one.component.html',
-  styleUrls: ['./step-one.component.scss']
+  styleUrls: ['./step-one.component.scss'],
 })
-export class StepOneComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class StepOneComponent {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
 }
