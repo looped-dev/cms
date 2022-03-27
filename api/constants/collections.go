13ff0686b664d420212cd1c49a6d86c5fd14ff35
Maina Wycliffe <wmmaina7@gmail.com>
2022-03-27 12:37:09 +0300

    Add Constants for Viper and Mongo Collections

--- api/constants/collections.go
+++ api/constants/collections.go
@@ -1,3 +1,10 @@
 package constants
 
+// the default database to use, if none is found in the configurations
+const DefaultDBName = "cms"
+
+// the name of the collection for storing settings
 const SettingsCollectionName = "settings"
+
+// the name of the mongodb collection for refresh tokens
+const StaffRefreshTokenCollection = "staff_refresh_tokens"
---
+++ api/constants/viper.go
@@ -0,0 +1,6 @@
+// Package constants stores a list of variables that are used throughout the code
+package constants
+
+// The name of the viper config key for the MongoDB database to
+// use for the CMS DATA
+const VIPER_DB_NAME = "MONGO_DB_NAME"
