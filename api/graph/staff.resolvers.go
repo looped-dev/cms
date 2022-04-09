fa0d8bd3de163a5da8564f7ee754273251e75628
Maina Wycliffe <wmmaina7@gmail.com>
2022-04-10 01:23:28 +0300

    Iimplement StaffRefreshToken Mutation Method

--- api/graph/staff.resolvers.go
+++ api/graph/staff.resolvers.go
@@ -7,10 +7,13 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/looped-dev/cms/api/auth"
+	"github.com/looped-dev/cms/api/constants"
 	"github.com/looped-dev/cms/api/graph/generated"
 	"github.com/looped-dev/cms/api/graph/model"
 	"github.com/looped-dev/cms/api/models"
 	"github.com/looped-dev/cms/api/staff"
+	"github.com/looped-dev/cms/api/utils"
 )
 
 func (r *mutationResolver) StaffLogin(ctx context.Context, input model.StaffLoginInput) (*model.StaffLoginResponse, error) {
@@ -63,7 +66,54 @@ func (r *mutationResolver) StaffLogout(ctx context.Context) (bool, error) {
 }
 
 func (r *mutationResolver) StaffRefreshToken(ctx context.Context, input model.StaffRefreshTokenInput) (*model.StaffLoginResponse, error) {
-	panic(fmt.Errorf("not implemented"))
+	refreshTokenRepository := auth.NewStaffRefreshToken(r.DB)
+	// get user from context
+	user := ctx.Value(constants.CurrentlyAuthenticatedUserContextKey)
+	if user == nil {
+		return nil, utils.NewGraphQLErrorWithError(401, fmt.Errorf("Access Denied!"))
+	}
+	userDetails, ok := user.(auth.StaffJWTClaims)
+	if !ok {
+		return nil, utils.NewGraphQLErrorWithError(500, fmt.Errorf("Internal Error"))
+	}
+	refreshToken, err := refreshTokenRepository.VerifyStaffRefreshToken(ctx, userDetails.ID, input.RefreshToken)
+	if err != nil {
+		return nil, utils.NewGraphQLErrorWithError(500, fmt.Errorf("Error validating refresh token: %v", err))
+	}
+	// invalidate token and create a new one
+	_, err = refreshTokenRepository.InvalidateRefreshToken(ctx, refreshToken)
+	if err != nil {
+		return nil, utils.NewGraphQLError(500,
+			fmt.Sprintf("Error validating refresh token: %v", err),
+		)
+	}
+	// create new session
+	jwt := auth.NewJWTRepository(r.DB)
+	userRepository := staff.NewStaffRepository(r.SMTPClient, r.DB)
+	staffMember, err := userRepository.FetchStaffFromDB(ctx, userDetails.Email)
+	if err != nil {
+		return nil, utils.NewGraphQLError(500,
+			fmt.Sprintf("Error fetching user details: %v", err),
+		)
+	}
+	newAccessToken, err := jwt.GenerateStaffAccessToken(staffMember)
+	if err != nil {
+		return nil, utils.NewGraphQLErrorWithError(500,
+			fmt.Errorf("Error generating access token: %v", err),
+		)
+	}
+	newRefreshToken, err := jwt.CreateStaffRefreshTokenSession(ctx, staffMember)
+	if err != nil {
+		return nil, utils.NewGraphQLErrorWithError(500,
+			fmt.Errorf("Error generating refresh token: %v", err),
+		)
+	}
+	newTokens := model.StaffLoginResponse{
+		AccessToken:  newAccessToken,
+		Staff:        staffMember,
+		RefreshToken: newRefreshToken,
+	}
+	return &newTokens, nil
 }
 
 func (r *staffResolver) Role(ctx context.Context, obj *models.StaffMember) (models.StaffRole, error) {
