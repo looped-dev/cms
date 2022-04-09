1e7c19ef66661dd89d3605f8961e18001eabebc1
Maina Wycliffe <wmmaina7@gmail.com>
2022-04-09 12:41:18 +0300

    Remove Duplicate State Directories

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/auth.module';
 export * from './lib/staff-must-be-logged-in.guard';
 export * from './lib/staff-must-not-be-logged-in.guard';
-export * from './lib/state/state/index';
+export * from './lib/state/index';
