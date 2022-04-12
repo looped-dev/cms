62f7cf1fda9507bd78288b1fda90d827dd132453
Maina Wycliffe <wmmaina7@gmail.com>
2022-04-12 23:19:58 +0300

    Fix Linting Errors

--- api/constants/context.go
+++ api/constants/context.go
@@ -3,4 +3,4 @@ package constants
 type ContextKey string
 
 const CurrentlyAuthenticatedUserContextKey ContextKey = "CURRENT_USER_CONTEXT_KEY"
-const BearerAuthorizationHeader = "BearerAuthorizationHeader"
+const BearerAuthorizationHeader ContextKey = "BearerAuthorizationHeader"
