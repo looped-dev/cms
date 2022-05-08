8f6de6c09a3fd17c26245664a7d4d4c7d642e59e
Maina Wycliffe <wmmaina7@gmail.com>
2022-05-08 23:17:38 +0300

    Install CDK and Setup Accordion Package for CDK

--- libs/pages/settings/src/lib/pages-settings.module.ts
+++ libs/pages/settings/src/lib/pages-settings.module.ts
@@ -4,6 +4,11 @@ import { RouterModule, Route } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { GeneralSettingsComponent } from './components/general-settings/general-settings.component';
+import { SeoComponent } from './components/seo/seo.component';
+import { TwitterComponent } from './components/twitter/twitter.component';
+import { FacebookComponent } from './components/facebook/facebook.component';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 export const pagesSettingsRoutes: Route[] = [
   {
@@ -18,7 +23,14 @@ export const pagesSettingsRoutes: Route[] = [
     RouterModule.forChild(pagesSettingsRoutes),
     FormsModule,
     FontAwesomeModule,
+    CdkAccordionModule,
   ],
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    GeneralSettingsComponent,
+    SeoComponent,
+    TwitterComponent,
+    FacebookComponent,
+  ],
 })
 export class PagesSettingsModule {}
